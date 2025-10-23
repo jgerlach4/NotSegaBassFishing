984cf243381609b0225cf5a484338dc83ea349f1
NJBurr <nburr@cedarville.edu>
2025-10-23 15:39:27 -0400

    got idle to walking done

--- Cedar Fishing/Assets/scripts/AnimationStateController.cs
+++ Cedar Fishing/Assets/scripts/AnimationStateController.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimationStateController : MonoBehaviour
 {
+    Animator animator;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Keyboard.current != null && Keyboard.current.wKey.isPressed)
+        {
+            animator.SetBool("isWalking", true);
+        }
         
+        if (Keyboard.current == null || !Keyboard.current.wKey.isPressed)
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 }
